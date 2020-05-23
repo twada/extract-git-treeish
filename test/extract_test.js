0967e176fea93c57a56e031d4748b531836d1818
Takuto Wada <takuto.wada@gmail.com>
2020-05-24 04:52:42 +0900

    refactor: tests as API document

--- test/extract_test.js
+++ test/extract_test.js
@@ -15,7 +15,7 @@ const fail = (args) => {
   assert(false, 'should not be here');
 };
 
-describe('extract-git-treeish', () => {
+describe('extract({ treeIsh, dest, [gitRoot], [spawnOptions] })', () => {
   let targetDir;
   beforeEach(() => {
     targetDir = path.join(os.tmpdir(), ymd());
@@ -26,62 +26,68 @@ describe('extract-git-treeish', () => {
     }
   });
 
-  it('resolves object when succeeded', () => {
-    return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
-      assert.deepStrictEqual(result, {
-        treeIsh: 'initial',
-        dir: targetDir
+  describe('returns `Promise` which will:', () => {
+    it('resolve with object containing `{treeIsh, dir}` when succeeded', () => {
+      return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
+        assert.deepStrictEqual(result, {
+          treeIsh: 'initial',
+          dir: targetDir
+        });
+      });
+    });
+
+    it('extract tree-ish content into `dest` on resolve', () => {
+      assert(!fs.existsSync(targetDir));
+      return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
+        assert(fs.existsSync(path.join(targetDir, '.gitignore')));
+        assert(fs.existsSync(path.join(targetDir, 'package.json')));
+        assert(!fs.existsSync(path.join(targetDir, 'index.js')));
+      });
+    });
+
+    it('reject with Error when tree-ish specified by `treeIsh` does not exist', () => {
+      return extract({ treeIsh: 'nonexistent', dest: targetDir }).then(fail, (err) => {
+        assert(err);
+        assert(err.message === 'Specified <tree-ish> does not exist [nonexistent]');
       });
     });
   });
 
-  it('extract tree-ish content into `dest`', () => {
-    assert(!fs.existsSync(targetDir));
-    return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
-      assert(fs.existsSync(path.join(targetDir, '.gitignore')));
-      assert(fs.existsSync(path.join(targetDir, 'package.json')));
-      assert(!fs.existsSync(path.join(targetDir, 'index.js')));
+  context('when `dest` does not exist:', () => {
+    it('creates `dest` recursively then resolves as usual', () => {
+      assert(!fs.existsSync(targetDir));
+      return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
+        assert(fs.existsSync(targetDir));
+      });
     });
   });
 
-  it('creates `dest` if does not exist', () => {
-    assert(!fs.existsSync(targetDir));
-    return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
-      assert(fs.existsSync(targetDir));
+  context('when `dest` already exists:', () => {
+    it('resolves as usual when `dest` is empty', () => {
+      fs.mkdirSync(targetDir);
+      assert(!fs.existsSync(path.join(targetDir, '.gitignore')));
+      assert(!fs.existsSync(path.join(targetDir, 'package.json')));
+      return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
+        assert(fs.existsSync(path.join(targetDir, '.gitignore')));
+        assert(fs.existsSync(path.join(targetDir, 'package.json')));
+      });
     });
-  });
 
-  it('resolves when `dest` is empty', () => {
-    fs.mkdirSync(targetDir);
-    assert(!fs.existsSync(path.join(targetDir, '.gitignore')));
-    assert(!fs.existsSync(path.join(targetDir, 'package.json')));
-    return extract({ treeIsh: 'initial', dest: targetDir }).then((result) => {
-      assert(fs.existsSync(path.join(targetDir, '.gitignore')));
-      assert(fs.existsSync(path.join(targetDir, 'package.json')));
+    it('rejects with Error when `dest` is not empty', () => {
+      fs.mkdirSync(targetDir);
+      touchSync(path.join(targetDir, 'foo'));
+      return extract({ treeIsh: 'initial', dest: targetDir }).then(fail, (err) => {
+        assert(err);
+        assert(err.message === `Specified <dest> is not empty [${targetDir}]`);
+      });
     });
-  });
 
-  it('rejects when `dest` is not empty', () => {
-    fs.mkdirSync(targetDir);
-    touchSync(path.join(targetDir, 'foo'));
-    return extract({ treeIsh: 'initial', dest: targetDir }).then(fail, (err) => {
-      assert(err);
-      assert(err.message === `Specified <dest> is not empty [${targetDir}]`);
-    });
-  });
-
-  it('rejects when treeIshName does not exist', () => {
-    return extract({ treeIsh: 'nonexistent', dest: targetDir }).then(fail, (err) => {
-      assert(err);
-      assert(err.message === 'Specified <tree-ish> does not exist [nonexistent]');
-    });
-  });
-
-  it('rejects when failed to create `dest` (ex. when `dest` is a file)', () => {
-    touchSync(targetDir);
-    return extract({ treeIsh: 'initial', dest: targetDir }).then(fail, (err) => {
-      assert(err);
-      assert(err.code === 'ENOTDIR');
+    it('rejects with Error when `dest` is not writable or not a directory', () => {
+      touchSync(targetDir);
+      return extract({ treeIsh: 'initial', dest: targetDir }).then(fail, (err) => {
+        assert(err);
+        assert(err.code === 'ENOTDIR');
+      });
     });
   });
 });
