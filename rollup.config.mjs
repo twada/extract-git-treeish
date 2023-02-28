799295f2643d7dcf4cc1423c65ca210fac372fa2
Takuto Wada <takuto.wada@gmail.com>
2023-02-28 09:29:02 +0900

    chore: ts -> mjs by tsc, mjs -> cjs by rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,14 @@
 import {promises as fs} from 'fs';
 
-const srcFiles = await fs.readdir(new URL('./src', import.meta.url));
+const distDir = await fs.readdir(new URL('./dist', import.meta.url));
 
 export default {
-  input: srcFiles.filter((file) => file.endsWith('.mjs')).map((x) => `src/${x}`),
+  input: distDir.filter((file) => file.endsWith('.mjs')).map((x) => `dist/${x}`),
   output: {
     dir: 'dist',
     format: 'cjs',
-    entryFileNames: '[name].cjs',
+    entryFileNames: '[name].js',
+    // entryFileNames: '[name].cjs',
     // create a module for each module in the input, instead of trying to chunk them together.
     preserveModules: true,
     // do not add `Object.defineProperty(exports, '__esModule', { value: true })`
